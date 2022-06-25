90ba5401384a1a68147bedc1c16fb0fb10179b8a
unknown <mohamedfathy573@gmail.com>
2022-06-25 11:00:33 +0200

    modify-routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExercisesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ExercisesController::class,'index']);
+
+
+Route::post('/store/exercises',[ExercisesController::class,'store_exercises']);
